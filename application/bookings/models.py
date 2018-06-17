dc5729402f6938621b04d9b9d37b0418e92becb7
Heidi <heidi.hassinen@helsinki.fi>
2018-06-17 21:04:17 +0300

    herokuun

--- application/bookings/models.py
+++ application/bookings/models.py
@@ -32,7 +32,6 @@ class Booking(Base):
                     " AND association.accomodation_id = Accomodation.id"
                     " AND Booking.accomodation_id = Accomodation.id"
                     " AND Accomodation.destination_id = Destination.id"
-                    " GROUP BY Booking.id, RoomType.name"
                     " ORDER BY Booking.date_created").params(client=client_id, approved=approved)
         res = db.engine.execute(stmt)
 
@@ -52,7 +51,6 @@ class Booking(Base):
                     " AND association.accomodation_id = Accomodation.id"
                     " AND Booking.accomodation_id = Accomodation.id"
                     " AND Accomodation.destination_id = Destination.id"
-                    " GROUP BY Booking.id, RoomType.name"
                     " ORDER BY Booking.date_created")
         res = db.engine.execute(stmt)
 
