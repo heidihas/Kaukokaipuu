ee02bf480188ecdcbc8156397f99604b69ee6f45
Heidi <heidi.hassinen@helsinki.fi>
2018-06-22 17:35:05 +0300

    viilausta

--- application/roomtypes/forms.py
+++ application/roomtypes/forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField, IntegerField, BooleanField, validators
 
 class RoomTypeForm(FlaskForm):
     name = StringField("Room-type name", [validators.Length(min=2), validators.Length(max=20)], render_kw={"class":"form-control"})
-    size = IntegerField("Person(s)", [validators.NumberRange(message='Value should be between 1 and 6.', min=1, max=6)], render_kw={"class":"form-control"})
+    size = IntegerField("Persons", [validators.NumberRange(message='Value should be between 1 and 6.', min=1, max=6)], render_kw={"class":"form-control"})
     price = IntegerField("Price/night", [validators.NumberRange(message='Value should be between 1 and 1000.', min=1, max=1000)], render_kw={"class":"form-control"})
     many = IntegerField("Rooms/hotel", [validators.NumberRange(message='Value should be at least 1.', min=1)], render_kw={"class":"form-control"})
 
@@ -18,7 +18,7 @@ class RoomTypeForm(FlaskForm):
 
 class RoomTypeChangeForm(FlaskForm):
     name = StringField("New name", [validators.Length(min=2), validators.Length(max=20)], render_kw={"class":"form-control"})
-    size = IntegerField("New person(s) number", [validators.NumberRange(message='Value should be between 1 and 6.', min=1, max=6)], render_kw={"class":"form-control"})
+    size = IntegerField("New persons number", [validators.NumberRange(message='Value should be between 1 and 6.', min=1, max=6)], render_kw={"class":"form-control"})
     price = IntegerField("New price/night", [validators.NumberRange(message='Value should be between 1 and 1000.', min=1, max=1000)], render_kw={"class":"form-control"})
     many = IntegerField("Rooms/hotel", [validators.NumberRange(message='Value should be at least 1.', min=1)], render_kw={"class":"form-control"})
 
